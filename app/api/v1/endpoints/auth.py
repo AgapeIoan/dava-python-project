72056fbd45bd194286f054e98024cde36d269ddc
Agape Ioan <ioanagape14@gmail.com>
2025-07-28 15:00:19 +0300

    Fixat lint

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from app.db.database import get_db
 from app.db.models import User
-from app.api.v1.schemas import UserCreate, UserLogin, Token
+from app.api.v1.schemas import UserCreate, Token
 from app.core.security import get_password_hash, verify_password, create_access_token
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
