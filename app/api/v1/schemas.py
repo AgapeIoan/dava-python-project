1084f6692822cc486d568ec3d310b23d186ee121
suliuliuc <serafim_u@yahoo.com>
2025-07-30 11:09:12 +0300

    refactoring for GET

--- app/api/v1/schemas.py
+++ app/api/v1/schemas.py
@@ -4,17 +4,6 @@ from datetime import datetime
 class MathResponse(BaseModel):
     result: float|complex
 
-class FibonacciRequest(BaseModel):
-    n: int = Field(..., ge=0, le=90, description="The index of the Fibonacci number (0 <= n <= 90).")
-
-class PowerRequest(BaseModel):
-    base: float|complex = Field(description='The base number for exponentiation. Can be real or complex.')
-    exponent: float|complex = Field(description='The exponent for the base number. Can be real or complex.')
-
-class FactorialRequest(BaseModel):
-    # Folosim Field pentru validari mai avansate, direct in schema
-    n: int = Field(..., ge=0, le=20, description="The number for the factorial (0 <= n <= 20).")
-
 class ApiKeyCreate(BaseModel):
     expires_in_seconds: int
 
@@ -32,9 +21,6 @@ class UserCreate(BaseModel):
     email: EmailStr
     password: str
 
-class UserLogin(BaseModel):
-    username: str
-    password: str
 
 class Token(BaseModel):
     access_token: str
