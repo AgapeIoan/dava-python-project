798241d3ffd74010939b4f29e8f26fd9f9c8f855
suliuliuc <serafim_u@yahoo.com>
2025-08-01 10:01:36 +0300

    enable connection pooling

--- app/db/database.py
+++ app/db/database.py
@@ -3,8 +3,12 @@ from sqlalchemy.orm import declarative_base
 
 from app.core.config import settings
 
-ASYNC_DATABASE_URL = settings.DATABASE_URL.replace("sqlite://", "sqlite+aiosqlite://")
-engine = create_async_engine(ASYNC_DATABASE_URL)
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    pool_size=10,  # Adjust pool size as needed
+    max_overflow=20,  # Allow some overflow connections
+    pool_pre_ping=True,  # Check connections before using them
+)
 
 AsyncSessionLocal = async_sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False #keeps the data accessible after commit
